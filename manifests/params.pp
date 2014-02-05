53839821fd0c38d5ef2f478ea048cbe4f5a76c3a
Elzair <elzairthesorcerer@gmail.com>
2014-02-05 17:08:15 -0500

    Synchronized parameters between config_elzair and config_elzair::params

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,7 @@ class config_elzair::params
   $user = "vagrant"
   $group = "vagrant"
   $home_dir = "/home/vagrant"
-  $root_dir = "/root"
+  $vagrant_dir = "/root"
   $operating_system = "linux"
   $distro = "ubuntu"
 }
